d80ba581b0ca94e4d02e4bc9e0c0778dc97751ee
Mperezblazquez <manuelperezblazquez93@gmail.com>
2015-03-17 19:14:08 +0100

    VS2013

---
+++ racional/model/Racional.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace racionales.model
+{
+    class Racional
+    {
+        private int numerador;
+        private int denominador;
+
+        public Racional(int numerador, int denominador)
+        {
+            this.numerador = numerador;
+            this.denominador = denominador;
+        }
+
+        public int getNumerador()
+        {
+            return this.numerador;
+        }
+
+        public void setNumerador(int numerador)
+        {
+            this.numerador = numerador;
+        }
+
+        public int getDenominador()
+        {
+            return this.denominador;
+        }
+
+        public Racional setDenominador(int denominador)
+        {
+            if (denominador == 0)
+            {
+                throw new Exception("Error: Prohibido poner 0");
+            }
+            this.numerador = numerador;
+        }
+
+        // multiplicar y dividir
+        public Racional multiplicar(Racional r)
+        {
+            int numerador = this.numerador * r.getNumerador();
+            int denominador = this.denominador * r.getDenominador();
+
+            return new Racional(numerador, denominador);
+        }
+
+        public Racional dividir(Racional r)
+        {
+            int numerador = this.numerador / r.getNumerador;
+            int denominador = this.denominador / r.getDenominador;
+
+            return new Racional(numerador, denominador);
+        }
+    }
+}
+
+
+
